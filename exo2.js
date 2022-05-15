cd89d493707f2d30878b85e5597aeebefeb86d2b
khaledmou20 <102072526+khaledmou20@users.noreply.github.com>
2022-05-15 13:01:47 +0100

    Delete exo2.js

--- exo2.js
+++
@@ -1,8 +0,0 @@
-var unirest = require("unirest")
- unirest
- .post('http://localhost:3000/authors')
- .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
- .send({"INSPIRE_number":"INSPIRE_1245","affiliation":"Univ","name":'{"first_name":"hammadi","last_name":"mahmoud"}',"title":"Mosta"})
- .then((response) => {
- console.log(response.body)
- });
